c965c3a7c469d5d10424aa55c226abc92ad89df3
tonesto7 <tonesto7@gmail.com>
2018-10-26 18:23:05 -0400

    deploy

--- alexa-cookie/alexa-cookie.js
+++ alexa-cookie/alexa-cookie.js
@@ -478,12 +478,12 @@ function initAmazonProxy(_options, email, password, callbackCookie, callbackList
                 proxyRes.headers['set-cookie'][i] = proxyRes.headers['set-cookie'][i].replace('Secure;', '');
             }
         }
-        let test1 = proxyRes.headers.location;
-        let test2 = proxyRes.socket.parser.outgoing.path;
-        let test3 = proxyRes.socket.parser.outgoing._headers.location || '';
-        console.log('test1:', test1);
-        console.log('test2:', test2);
-        console.log('test3:', test3);
+        // let test1 = proxyRes.headers.location;
+        // let test2 = proxyRes.socket.parser.outgoing.path;
+        // let test3 = proxyRes.socket.parser.outgoing._headers.location || '';
+        // console.log('test1:', test1);
+        // console.log('test2:', test2);
+        // console.log('test3:', test3);
 
         let chk1 = (
             proxyRes.socket !== undefined &&
