0e1a667b8f563bb557778a6d5795dbe9ae5a3512
tonesto7 <tonesto7@gmail.com>
2018-11-06 09:59:59 -0500

    enabled debug

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function loadConfig() {
     configFile.set('settings.useHeroku', (process.env.useHeroku === true || process.env.useHeroku === 'true'));
     configFile.set('settings.amazonDomain', process.env.amazonDomain || configData.settings.amazonDomain);
     configFile.set('settings.smartThingsUrl', process.env.smartThingsUrl || configData.settings.smartThingsUrl);
-    configFile.set('settings.serviceDebug', (process.env.serviceDebug === true));
+    configFile.set('settings.serviceDebug', true);
+    // configFile.set('settings.serviceDebug', (process.env.serviceDebug === true));
     configFile.set('settings.serverPort', process.env.PORT || (configData.settings.serverPort || 8091));
     configFile.set('settings.refreshSeconds', process.env.refreshSeconds ? parseInt(process.env.refreshSeconds) : (configData.settings.refreshSeconds || 60));
     if (!configData.state) {
