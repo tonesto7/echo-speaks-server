4a832633b450fdd568c668fb622e1b96c0286603
tonesto7 <tonesto7@gmail.com>
2021-10-15 11:33:25 -0400

    sync

--- index.js
+++ index.js
@@ -167,7 +167,7 @@ async function startWebConfig() {
 
                 logger.info(`CreateRS Request Received`);
                 if (devPK && adpTkn) {
-                    res.send(createRS(devPK, adpTkn));
+                    res.send({ rs: createRS(devPK, adpTkn) });
                 } else {
                     res.send("Missing Token or PK");
                 }
